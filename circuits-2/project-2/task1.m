e08a0164345f07190d80c9db8087e606b052865c
arhoadarmer <austin.rhoadarmer@hbesystems.com>
2018-09-23 16:45:54 -0500

    added project 2 tasks

---
+++ circuits-2/project-2/task1.m
@@ -0,0 +1,11 @@
+x = 10;
+y = 5;
+u = x > y
+v = x < y
+w = x == y
+ww = x >= 2*y
+if x > y
+    y = x;
+else
+    x = y;
+end
---
+++ circuits-2/project-2/task2.m
@@ -0,0 +1,45 @@
+clear all
+clc
+close all
+
+% Part 1
+T = .001;
+V_m = 1.0;
+t = linspace(0,T,1001);
+for n = 1:1001
+    v(n) = V_m - (V_m / T) * t(n);
+end
+figure(1);
+plot(t,v);
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Time vs Voltage');
+legend('v(t)= V_m - (V_m / T) * t');
+
+% Part 2
+t = linspace(-T,2*T,3001);
+for n = 1:3001
+    if (0 <= t(n) && t(n) <= T)
+        v(n) = V_m - (V_m / T) * t(n);
+    else
+        v(n) = 0;
+    end
+end
+figure(2);
+plot(t,v);
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Time vs Voltage');
+legend('v(t)= V_m - (V_m / T) * t');
+
+% Part 3
+t = linspace(-T,2*T,3001);
+for n = 1:3001
+    v(n) = V_m - (V_m / T) * mod(t(n),T);
+end
+figure(3);
+plot(t,v);
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Time vs Voltage');
+legend('v(t)= V_m - (V_m / T) * mod(t,T)');
